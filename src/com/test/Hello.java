29ccb300ace8d0920d48985e1be45a2b76e4f735
cpj <15701153915@sina.cn>
2018-04-07 21:35:10 +0800

    first commit

---
+++ src/com/test/Hello.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2018/4/7.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("打分支前");
+        System.out.print("Hello Word!!!");
+    }
+}
