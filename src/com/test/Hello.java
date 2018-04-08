b2a294e3343fb3bda6de51c1f85c19088ac0f348
15701153915@sina.cn <15701153915qq>
2018-04-08 22:07:57 +0800

    oooooo

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -5,8 +5,8 @@ package com.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("打分支前----------------------");
-        System.out.print("Hello Word*****************!!!");
-        System.out.print("打分支后的文件********");
+        System.out.print("11111111111111111111");
+        System.out.print("222222222222222222222");
+        System.out.print("333333333333333333333");
     }
 }
