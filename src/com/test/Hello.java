26d7c9fc32e8024366d6bea31b7b58dfecc1ec53
15701153915@sina.cn <15701153915qq>
2018-04-15 16:28:17 +0800

    99999999999

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -5,8 +5,8 @@ package com.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("我叫车配江");
-        System.out.print("我叫车配江");
-        System.out.print("我叫车配江");
+        System.out.print("9999999");
+        System.out.print("9999999");
+        System.out.print("9999999");
     }
 }
